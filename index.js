cf6dce45b3d53f0c1410a0a8838824ee88287225
gorillaRouge <john.schoppmann@yahoo.com>
2023-01-11 14:01:56 -0700

    added code getting ready for testing

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project:"
+  },
+  {
+    type: "input",
+    name: "video",
+    message: "Enter a YouTube video ID for your project (if applicable):"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide instructions on how to install your project:"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide examples and instructions on how to use your project:"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Provide instructions on how to contribute to your project:"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide instructions on how to run tests for your project:"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "GPLv3", "Apache", "none"]
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+  // Use the `fs` package to write the file
+  fs.writeFile(fileName, data, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Successfully created README.md file!");
+    console.log(`Your ${README_FILENAME} has been generated at ${fullReadmePath}! 🚀`)
+  });
+};
 
-// Function call to initialize app
-init();
+// Create a function to initialize app
+function init() {
+  // Use the `inquirer` package to prompt the user for input
+  inquirer.prompt(questions).then(function(answers) {
+    // Call the `generateMarkdown` function to generate the markdown
+    const markdown = generateMarkdown(answers);
+    // Call the `writeToFile` function to write the file
+    writeToFile("README.md", markdown);
+  });
+};
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+/* // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -14,6 +14,96 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+`;
+} */
+
+/* 
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  const badgeUrl = `https://img.shields.io/badge/license-${license}-blue`;
+  return `![License](${badgeUrl})`;
+}
+
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  const link = `https://choosealicense.com/licenses/${license}`;
+  return `[${license}](${link})`;
+}
+
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+  return `## License
+
+${badge}
+
+This project is licensed under the ${link} license.`;
+} */
+
+const gravatar = require('gravatar');
+
+const readMe_size = 200;
+
+function getLicenseLink({ license }) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+
+    case 'GPL':
+      return 'https://www.gnu.org/licenses/gpl.html';
+
+    case 'ISC':
+      return 'https://opensource.org/licenses/ISC';
+
+    case 'MPL':
+      return 'https://www.mozilla.org/en-US/MPL/';
+
+    case 'AGPL':
+      return 'https://www.gnu.org/licenses/agpl.html';
+
+    default:
+      return 'https://opensource.org/licenses/';
+  }
+}
+
+function generateVideo({ youtube_id, name }) {
+  if (youtube_id) {
+    return `
+### Video - ${name}
+[![Youtube Video, how to use ${name}](https://img.youtube.com/vi/${youtube_id}/0.jpg)](https://www.youtube.com/watch?v=${youtube_id})
+`;
+  }
+
+  return '';
+}
+
+function generateMarkdown(data) {
+  const video = generateVideo({ youtube_id: data.video, name: data.title });
+  const profilePhotoUrl = gravatar.url(data.email, { protocol: 'https', s: readMe_size });
+  const licenseLink = getLicenseLink(data);
+
+  return `
+# ${data.name}
+### *${data.heading}*
+## 📄 Description
+${data.description}
+## ⚙️ Requirements
+${data.requirements}
+## 💻 Usage
+${data.usage}
+${video}
+## 👨‍💻 Who made it?
+[![${data.author}](${profilePhotoUrl})](${data.website} "${data.author} personal website")
+**[${data.author}](${data.website})**, a highly passionate, focused; pragmatic software engineer 🧙‍♂️
+## ⚖️ License
+**${data.name}** is generously distributed under the *[${data.license}](${licenseLink})*.
 `;
 }
 
